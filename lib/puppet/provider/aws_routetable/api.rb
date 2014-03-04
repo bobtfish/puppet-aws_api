3c72938b2c09d4dee1ab57f7b8c4b0f8edd3e9fd
Tomas Doran <tdoran@yelp.com>
2014-03-04 16:58:46 +0000

    Add empty providers for the other types

---
+++ lib/puppet/provider/aws_routetable/api.rb
@@ -0,0 +1,16 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', 'puppet_x', 'bobtfish', 'ec2_api.rb'))
+
+Puppet::Type.type(:aws_routetable).provide(:api, :parent => Puppet_X::Bobtfish::Ec2_api) do
+  mk_resource_methods
+  def self.instances
+    []
+  end
+  def exists?
+    @property_hash[:ensure] == :present
+  end
+  def create
+  end
+  def destroy
+  end
+end
+
