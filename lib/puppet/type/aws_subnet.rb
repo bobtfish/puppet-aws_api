9a2b4eb0e1fb7d7fb86045384d668e4e46d6e813
Tomas Doran <tdoran@yelp.com>
2014-03-04 07:14:56 -0800

    Fix validation

--- lib/puppet/type/aws_subnet.rb
+++ lib/puppet/type/aws_subnet.rb
@@ -2,7 +2,14 @@ Puppet::Type.newtype(:aws_subnet) do
   @doc = "Manage AWS subnets"
   newparam(:name)
   ensurable
-  newproperty(:vpc_id)
+  newproperty(:vpc_id) do
+  end
+  autorequire(:aws_vpc) do
+    self[:vpc_id]
+  end
+  validate do
+    fail('vpc_id is required') if self[:vpc_id].nil?
+  end
   newproperty(:cidr)
   newproperty(:az)
   newproperty(:route_table)
