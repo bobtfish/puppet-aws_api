6d4cd0f2730213c04567069291e420bc911b9007
Tomas Doran <tdoran@yelp.com>
2014-03-04 09:49:13 +0000

    Remove all the standard module stuff

--- manifests/config.pp
+++
@@ -1,6 +0,0 @@
-# == Class aws_api::config
-#
-# This class is called from aws_api
-#
-class aws_api::config {
-}
--- manifests/install.pp
+++
@@ -1,9 +0,0 @@
-# == Class aws_api::install
-#
-class aws_api::install {
-  include aws_api::params
-
-  package { $aws_api::params::package_name:
-    ensure => present,
-  }
-}
