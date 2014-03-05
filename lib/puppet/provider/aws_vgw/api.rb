96236b0a139e4c0415449606cdd6dcf6ae7baa5a
Tomas Doran <tdoran@yelp.com>
2014-03-04 16:21:58 -0800

    Get puppet resource working for vgws

--- lib/puppet/provider/aws_vgw/api.rb
+++ lib/puppet/provider/aws_vgw/api.rb
@@ -2,15 +2,46 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', 'pu
 
 Puppet::Type.type(:aws_vgw).provide(:api, :parent => Puppet_X::Bobtfish::Ec2_api) do
   mk_resource_methods
+
+  def self.new_from_aws(region_name, item)
+    tags = item.tags.to_h
+    name = tags.delete('Name') || item.id
+    new(
+      :aws_item         => item,
+      :name             => name,
+      :id               => item.id,
+      :region           => region_name,
+      :ensure           => :present,
+      :tags             => tags
+    )
+  end
   def self.instances
-    []
+    regions.collect do |region_name|
+      ec2.regions[region_name].vpn_gateways.collect { |item| new_from_aws(region_name,item) }
+    end.flatten
   end
   def exists?
     @property_hash[:ensure] == :present
   end
+  [:ip_address, :bgp_asn, :region].each do |ro_method|
+    define_method("#{ro_method}=") do |v|
+      fail "Cannot manage #{ro_method} is read-only once an vgw is created"
+    end
+  end
   def create
+    begin
+      vgw = ec2.regions[resource[:region]].vpn_gateways.create(resource[:bgp_asn].to_i, resource[:ip_address])
+      tag_with_name vgw, resource[:name]
+      tags = resource[:tags] || {}
+      tags.each { |k,v| vgw.add_tag(k, :value => v) }
+      vgw
+    rescue Exception => e
+      fail e
+    end
   end
   def destroy
+    @property_hash[:aws_item].delete
+    @property_hash[:ensure] = :absent
   end
 end
 
--- lib/puppet/type/aws_vgw.rb
+++ lib/puppet/type/aws_vgw.rb
@@ -2,15 +2,6 @@ Puppet::Type.newtype(:aws_vgw) do
   @doc = "Manage AWS virtual private gateways"
   newparam(:name)
   ensurable
-  newproperty(:vpc_id) do
-  end
-  autorequire(:aws_vpc) do
-    self[:vpc_id]
-  end
-  validate do
-    fail('vpc_id is required') if self[:vpc_id].nil?
-  end
-  newproperty(:type)
   newproperty(:tags)
 end
 
