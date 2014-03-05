c2b79df78968820f296a8acccd1ea3400c31c857
Tomas Doran <tdoran@yelp.com>
2014-03-05 01:42:22 -0800

    Fix tests

--- spec/unit/puppet/aws_dopts_provider_spec.rb
+++ spec/unit/puppet/aws_dopts_provider_spec.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env rspec
 require 'spec_helper'
 
-provider_class = Puppet::Type.type(:aws_ugw).provider(:api)
+provider_class = Puppet::Type.type(:aws_dopts).provider(:api)
 
 describe provider_class do
 
--- spec/unit/puppet/aws_igw_provider_spec.rb
+++ spec/unit/puppet/aws_igw_provider_spec.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env rspec
 require 'spec_helper'
 
-provider_class = Puppet::Type.type(:aws_dopt).provider(:api)
+provider_class = Puppet::Type.type(:aws_igw).provider(:api)
 
 describe provider_class do
 
