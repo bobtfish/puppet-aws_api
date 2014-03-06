050838772d45df5b853a3a759ac169eeefdd8e20
Tomas Doran <tdoran@yelp.com>
2014-03-06 02:01:41 +0000

    Start trying to set/reset groups

--- lib/puppet/provider/aws_iam_user/api.rb
+++ lib/puppet/provider/aws_iam_user/api.rb
@@ -1,4 +1,5 @@
 require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', 'puppet_x', 'bobtfish', 'ec2_api.rb'))
+require 'set'
 
 Puppet::Type.type(:aws_iam_user).provide(:api, :parent => Puppet_X::Bobtfish::Ec2_api) do
   mk_resource_methods
@@ -16,6 +17,17 @@ Puppet::Type.type(:aws_iam_user).provide(:api, :parent => Puppet_X::Bobtfish::Ec
   end
   def self.instances
     iam.users.collect { |item| new_from_aws(item) }
+  end
+  [:arn, :path, :name].each do |ro_method|
+    define_method("#{ro_method}=") do |v|
+      fail "Cannot manage #{ro_method} is read-only once a user is created"
+    end
+  end
+  def groups=(newgroups)
+    groups_to_add = Set.new(newgroups).subtract(@property_hash[:groups]).to_a.map { |name| iam.groups[name] }
+    groups_to_remove = Set.new(@property_hash[:groups]).subtract(newgroups).to_a.map { |name| iam.groups[name] }
+    puts groups_to_add.inspect
+    puts groups_to_remove.inspect
   end
   def create
     begin
