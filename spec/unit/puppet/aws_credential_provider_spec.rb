f0dad110af849196548bf74dedb4b32f3c888213
Matt Carroll <mattc@yelp.com>
2014-03-31 13:34:47 +0100

    Added some more tests for credentials type

--- spec/unit/puppet/aws_credential_provider_spec.rb
+++ spec/unit/puppet/aws_credential_provider_spec.rb
@@ -1,15 +1,26 @@
 #!/usr/bin/env rspec
 require 'spec_helper'
 
-provider_class = Puppet::Type.type(:aws_credential).provider(:api)
+type_class = Puppet::Type.type(:aws_credential)
+provider_class = type_class.provider(:api)
 
 describe provider_class do
-  let(:instances) do
-    provider_class.instances
-  end
+  let(:instances) { provider_class.instances }
 
   it('does not have any instances') do
     expect(instances.size).to eql 0
+  end
+end
+
+describe type_class do
+  it "should be able to create an instance" do
+    described_class.new(:name => 'foo').should_not be_nil
+  end
+
+  [:access_key, :secret_key].each do |param|
+    it "should accept #{param} parameter" do
+      described_class.attrtype(param).should == :property
+    end
   end
 end
 
