676c51554dfebe946619ef59766891867613c0c1
Matt Carroll <mattc@yelp.com>
2014-03-31 17:05:43 +0100

    Removed unneeded dependencies

--- lib/puppet/provider/aws_credential/api.rb
+++ lib/puppet/provider/aws_credential/api.rb
@@ -1,6 +1,4 @@
-require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', 'puppet_x', 'bobtfish', 'ec2_api.rb'))
-
-Puppet::Type.type(:aws_credential).provide(:api, :parent => Puppet_X::Bobtfish::Ec2_api) do
+Puppet::Type.type(:aws_credential).provide(:api) do
   mk_resource_methods
 
   def self.instances
