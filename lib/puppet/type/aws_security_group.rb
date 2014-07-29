af0b69c6199a094ddf188d19385936637f8525b6
Ruy Asan <ruyasan@gmail.com>
2014-07-29 12:38:57 -0700

    i think this was eating my arrays?

--- lib/puppet/type/aws_security_group.rb
+++ lib/puppet/type/aws_security_group.rb
@@ -8,7 +8,9 @@ Puppet::Type.newtype(:aws_security_group) do
     self[:vpc]
   end
   newproperty(:tags)
-  newproperty(:authorize_ingress)
-  newproperty(:authorize_egress)
+  newproperty(:authorize_ingress) do
+  end
+  newproperty(:authorize_egress) do
+  end
 end
 
