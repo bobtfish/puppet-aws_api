bd07b2958ecc55fad0e268f08c4da03ab56530d3
Ruy Asan <ruyasan@gmail.com>
2014-08-18 16:51:53 -0700

    fixed issue with aws_s3_buckets and using us-east-1

--- lib/puppet/provider/aws_s3_bucket/api.rb
+++ lib/puppet/provider/aws_s3_bucket/api.rb
@@ -32,10 +32,14 @@ Puppet::Type.type(:aws_s3_bucket).provide(:api, :parent => Puppet_X::Bobtfish::E
   
 
   def create
-    s3(resource[:region]).buckets.create(resource[:name],
-      :location_constraint => resource[:region]
-    )
-    
+    s3_region_string = if resource[:region] == 'us-east-1'
+      # yep - this is a special problem with the S3 bucket API alone
+      s3(resource[:region]).buckets.create(resource[:name])
+    else
+      s3(resource[:region]).buckets.create(resource[:name],
+        :location_constraint => resource[:region]
+      )
+    end
   end
   def destroy
     aws_item.delete
