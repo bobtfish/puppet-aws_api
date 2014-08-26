43fbae92830c2a877ea9896aff55b4fff227dd5d
Ruy Asan <ruyasan@gmail.com>
2014-08-25 18:50:06 -0700

    only need 1st arg

--- lib/puppet/parser/functions/aws_azs.rb
+++ lib/puppet/parser/functions/aws_azs.rb
@@ -1,6 +1,6 @@
 require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', 'puppet_x', 'bobtfish', 'aws_api.rb'))
 module Puppet::Parser::Functions
   newfunction(:aws_azs) do |region_name|
-    Puppet_X::Bobtfish::Aws_api.ec2.regions[region_name].availability_zones.collect(&:name)
+    Puppet_X::Bobtfish::Aws_api.ec2.regions[region_name.first].availability_zones.collect(&:name)
   end
 end
