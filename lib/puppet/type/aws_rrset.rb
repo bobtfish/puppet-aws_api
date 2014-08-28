aa1729cc612569622bb97e3c770fe55707c2b203
Ruy Asan <ruyasan@gmail.com>
2014-08-28 11:01:28 -0700

    Fully fleshed out  validations and autorequire for Aws_rrset’s new generic targets property.

--- lib/puppet/type/aws_rrset.rb
+++ lib/puppet/type/aws_rrset.rb
@@ -1,12 +1,28 @@
+require 'puppetx/bobtfish/type_helpers'
 Puppet::Type.newtype(:aws_rrset) do
   @doc = "Manage AWS Route 54 resource record sets"
+
+  # Populated by name param during validation
+  attr_accessor :record_name, :record_type
+
   newparam(:name) do
     desc "Record type followed by name, space sparated (e.g. 'CNAME foo.example.com.')"
-    newvalues /^([A-Z]+)\s+([-a-z0-0\.]+)\.$/
+    validate do |value|
+       if value =~ /^([A-Z]+)\s+([-_a-z0-9\.]+\.)$/
+        resource.record_type = $1
+        resource.record_name = $2
+       else
+        raise ArgumentError, "Aws_rrset names must be in the form of 'CNAME foo.example.com.'"
+       end
+    end
+
   end
   ensurable
+
+
   newproperty(:zone) do
-      desc "A aws_hosted_zone name"
+      desc "An aws_hosted_zone name (required)"
+      include Puppetx::Bobtfish::RequiredValue
   end
   autorequire(:aws_hosted_zone) do
     self[:zone]
@@ -15,51 +31,47 @@ Puppet::Type.newtype(:aws_rrset) do
   newproperty(:value, :array_matching => :all) do
     defaultto []
     desc "The record value string (array of strings for multiple lines)"
-    def props
-        resource.provider.resource
-    end
-    def lookup_value
-        if props[:ec2_instance]
-            instance = resource.provider.lookup(:aws_ec2_instance, props[:ec2_instance])
-            # Value matches if ec2_instance public ip matches
-            return instance.elastic_ip.public_ip
-        elsif props[:load_balancer]
-            # Value matches if load_balancer dns_name matches
-            lb = resource.provider.lookup(:aws_elb, props[:load_balancer])
-            return lb.dns_name
-        end
-    end
-    def should_to_s(newvalue)
-        if newvalue.empty?
-            "#{[lookup_value]} (from #{props[:ec2_instance] or props[:load_balancer]})"
-        else
-            super(newvalue)
-        end
-    end
-    def insync?(is)
-        if should.empty?
-            # If no value given, use lookup value
-            is == [lookup_value]
-        else
-            # If we have an actual value, just look it up normally
-            super
-        end
+  end
+
+  VALID_TARGET_TYPES = [
+    :aws_ec2_instance,
+    :aws_elb,
+    :aws_cache_cluster,
+    :aws_s3_bucket
+  ]
+
+  VALID_TARGET_TYPES.each do|type|
+    autorequire(type) do
+      filter_targets(type.to_s.capitalize).collect(&:title).uniq
     end
   end
 
   newparam(:targets, :array_matching => :all) do
-    desc "A list of other AWS resources which will be used to fill out placeholders in the value strings."
-  end
-
-  autorequire(:aws_ec2_instance) do
-    self[:ec2_instance]
-  end
-  autorequire(:aws_elb) do
-    self[:load_balancer]
+    defaultto []
+    desc "An array of other AWS resources which will be used to fill out  placeholders in their corresponding value lines."
+    validate do |value|
+      value.each do |target|
+        unless target.is_a? Puppet::Resource
+          raise ArgumentError, "Aws_rrset targets must be direct references to another valid AWS resource."
+        end
+        unless VALID_TARGET_TYPES.include?(target.type.downcase.to_sym)
+          raise ArgumentError, "Only the following resources are valid targets for Aws_rrset: #{VALID_TARGET_TYPES.join(', ')}"
+        end
+      end
+    end
   end
 
   newproperty(:ttl) do
       desc "TTL in seconds"
+      include Puppetx::Bobtfish::EnsureIntValue
+  end
+
+  private
+
+  def filter_targets(type_name)
+    self[:targets].find_all do |target|
+      target.type  == type_name
+    end
   end
 
 end
