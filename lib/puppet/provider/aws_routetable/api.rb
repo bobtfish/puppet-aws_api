c78e083b0643481a76e1eac33dc2f7374fc717e8
Tomas Doran <tdoran@yelp.com>
2014-12-01 11:10:48 +0000

    Ruby 1.8.7 fix

--- lib/puppet/provider/aws_routetable/api.rb
+++ lib/puppet/provider/aws_routetable/api.rb
@@ -25,7 +25,7 @@ Puppet::Type.type(:aws_routetable).provide(:api, :parent => Puppet_X::Bobtfish::
           :network_interface => name_or_id(route.network_interface),
           :internet_gateway => name_or_id(route.internet_gateway)
         }.reject { |k, v| v.nil? } },
-      :propagate_routes_from => [],
+      :propagate_routes_from => []
     )
   end
   read_only(:vpc, :subnets, :routes, :main)
