459ccb8bbe71a1220454078fa1eeb7387c87daa3
Tomas Doran <tdoran@yelp.com>
2014-12-01 19:29:45 +0000

    Add a horrible hack to fix seeming brokenness in the amazon SDK

--- lib/puppet/provider/aws_subnet/api.rb
+++ lib/puppet/provider/aws_subnet/api.rb
@@ -1,5 +1,19 @@
 require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', 'puppet_x', 'bobtfish', 'ec2_api.rb'))
 
+# FIXME
+class AWS::EC2::SubnetCollection
+  def create cidr_block, options = {}
+    client_opts = {}
+    client_opts[:vpc_id] = vpc_id_option(options)
+    client_opts[:cidr_block] = cidr_block
+    client_opts[:availability_zone] = az_option(options) if
+      options[:availability_zone]
+
+    resp = client.create_subnet(client_opts)
+    resp[:subnet][:subnet_id]
+  end
+end
+
 Puppet::Type.type(:aws_subnet).provide(:api, :parent => Puppet_X::Bobtfish::Ec2_api) do
   mk_resource_methods
   remove_method :tags= # We want the method inherited from the parent
@@ -49,9 +63,11 @@ Puppet::Type.type(:aws_subnet).provide(:api, :parent => Puppet_X::Bobtfish::Ec2_
       resource[:az] = unused_azs.first
     end
 
-    subnet = vpc.subnets.create(resource[:cidr],
-        :availability_zone => resource[:az]
-    )
+    # FIXME
+    subnet_id = vpc.subnets.create(resource[:cidr], :availability_zone => resource[:az], :vpc => vpc)
+    sleep 2
+    subnet = find_vpc_item_by_name(resource[:vpc]).subnets[subnet_id]
+    # END FIXME
     wait_until_state subnet, :available
     tag_with_name subnet, resource[:name]
     tags = resource[:tags] || {}
